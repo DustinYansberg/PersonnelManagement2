b77574becea17fed7893c4031da30e08852aaecb
ChriSaenz <45044865+ChriSaenz@users.noreply.github.com>
2024-08-08 08:06:41 -0500

    Preparing for create and update for Accounts

--- spring-boot/personnelmgmt2/src/main/java/com/personnelmgmt2/models/Account.java
+++ spring-boot/personnelmgmt2/src/main/java/com/personnelmgmt2/models/Account.java
@@ -7,7 +7,7 @@ public class Account {
 
 	String accountAppId;	//	Required.
 	String accountUserId;	//	Required.
-	String nativeIdentity;	//	Required.
+	String nativeIdentity;	//	Required in the body, even if it's blank
 	String displayName;
 	String instanceId;		//	Cannot change once set.
 	String password;		//	Required?
@@ -18,8 +18,19 @@ public class Account {
 	boolean hasEntitlements;
 	
 	//	Because some accounts are for Salesforce and some are for Azure Entra ID.
-	String accountAppName;	//	Required. Is either "Salesforce" or "Azure Entra ID"
-	String appSpecificProperties;	//	"property": "value", "property": "value", ...
+	String accountAppName;	//	Required. Must either be "Salesforce" or "Azure Entra ID"
+	//	All of these are required for Salesforce apps.
+	String salesforceUsername;	//	MUST BE IN EMAIL FORM
+	String salesforceLastName;
+	String salesforceFirstName;
+	String salesforceCommunityNickname;
+	String salesforceAlias;	//	Alphanumeric, must be 8 characters long and unique.
+	String salesforceEmail;
+	static final String timeZoneSidKey = "America/Los_Angeles";
+	static final String localeSidKey = "en_US";
+	static final String emailEncodingKey = "UTF-8";
+	static final String languageLocaleKey = "en_US";
+	
 
 	public Account(String accountAppId, String accountUserId, String nativeIdentity, String displayName,
 			String instanceId, String password, String currentPassword, boolean active, boolean locked,
@@ -40,21 +51,37 @@ public class Account {
 	}
 
 	public String toJsonString() {
-		return "{\r\n"
-				+ "  \"identity\": {\r\n"
-				+ "    \"value\": \"" + accountUserId + "\"\r\n"
-				+ "  },\r\n"
-				+ "  \"application\": {\r\n"
-				+ "    \"value\": \"" + accountAppId + "\"\r\n"
-				+ "  },\r\n"
-				+ "  \"nativeIdentity\": \"" + nativeIdentity + "\",\r\n"
-				+ "  \"displayName\": \"" + displayName + "\",\r\n"
-				+ "  \"instance\": \"" + instanceId + "\",\r\n"
-				+ "  \"password\": \"" + password + "\",\r\n"
-				+ "  \"currentPassword\": \"" + currentPassword + "\",\r\n"
-				+ "  \"urn:ietf:params:scim:schemas:sailpoint:1.0:Application:Schema:" + accountAppName + ":account\": {" + appSpecificProperties + "},\r\n"
-				+ "  \"active\": " + active + ",\r\n"
-				+ "  \"locked\": " + locked + "\r\n"
-				+ "}";
+		String asJson = "{\r\n"
+			+ "  \"identity\": {\r\n"
+			+ "    \"value\": \"" + accountUserId + "\"\r\n"
+			+ "  },\r\n"
+			+ "  \"application\": {\r\n"
+			+ "    \"value\": \"" + accountAppId + "\"\r\n"
+			+ "  },\r\n"
+			+ "  \"nativeIdentity\": \"" + nativeIdentity + "\",\r\n"
+			+ "  \"displayName\": \"" + displayName + "\",\r\n"
+			+ "  \"instance\": \"" + instanceId + "\",\r\n"
+			+ "  \"password\": \"" + password + "\",\r\n"
+			+ "  \"currentPassword\": \"" + currentPassword + "\",\r\n"
+			+ "  \"urn:ietf:params:scim:schemas:sailpoint:1.0:Application:Schema:" + accountAppName + ":account\": {";
+		if(accountAppName.equals("Salesforce")) {
+			asJson = asJson
+				+ "    \"ProfileName\": \"Standard User\",\r\n"
+				+ "    \"Username\": \"" + salesforceUsername + "\",\r\n"
+				+ "    \"LastName\": \"" + salesforceLastName + "\",\r\n"
+				+ "    \"FirstName\": \"" + salesforceFirstName + "\",\r\n"
+				+ "    \"CommunityNickname\": \"" + salesforceCommunityNickname + "\",\r\n"
+				+ "    \"Alias\": \"" + salesforceAlias + "\",\r\n"
+				+ "    \"Email\": \"" + salesforceEmail + "\",\r\n"
+				+ "    \"TimeZoneSidKey\": \"" + timeZoneSidKey + "\",\r\n"
+				+ "    \"LocaleSidKey\": \"" + localeSidKey + "\",\r\n"
+				+ "    \"EmailEncodingKey\": \"" + emailEncodingKey + "\",\r\n"
+				+ "    \"LanguageLocaleKey\": \"" + languageLocaleKey + "\",\r\n";
+		}
+		asJson = asJson + "  },\r\n"
+			+ "  \"active\": " + active + ",\r\n"
+			+ "  \"locked\": " + locked + "\r\n"
+			+ "}";
+		return asJson;
 	}
 }
