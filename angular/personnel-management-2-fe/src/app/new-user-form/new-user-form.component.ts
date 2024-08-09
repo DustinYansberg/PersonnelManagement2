6948addb8de90a06d521588c860fb9da8e5a1569
DustinYansberg <DustinYansberg@gmail.com>
2024-08-09 04:36:04 -0700

    fixing validator issue so the submit button becomes enabled appropriately.

--- angular/personnel-management-2-fe/src/app/new-user-form/new-user-form.component.ts
+++ angular/personnel-management-2-fe/src/app/new-user-form/new-user-form.component.ts
@@ -1,78 +1,103 @@
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
-import { FormBuilder, FormGroup,  FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { HttpService } from '../services/http.service';
 import { User } from '../models/user';
 import { ErrorPipe } from '../pipes/error.pipe';
 import { Button, ButtonModule } from 'primeng/button';
-
-
 
 @Component({
   selector: 'app-new-user-form',
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule, CommonModule, ErrorPipe, Button],
   templateUrl: './new-user-form.component.html',
-  styleUrl: './new-user-form.component.css'
+  styleUrl: './new-user-form.component.css',
 })
 export class NewUserFormComponent {
-
   newUserForm: FormGroup;
-  user: User = new User('', '', '', '', '', '', '', '', '', '', '','','',true,'');
+  user: User = new User(
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    true,
+    ''
+  );
 
   constructor(
     private httpService: HttpService,
     private formBuilder: FormBuilder
   ) {
     this.newUserForm = this.formBuilder.group({
-      firstName: ['',Validators.compose([Validators.required, Validators.minLength(4)]),],
-      lastName: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      firstName: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(4)]),
+      ],
+      lastName: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(4)]),
+      ],
       username: ['', Validators.required],
       displayName: ['', Validators.required],
       email: ['', Validators.compose([Validators.required, Validators.email])],
       managerId: [''],
-      type: ['', Validators.required],
+      type: [''],
       userId: [''],
       password: ['', Validators.required],
       softwareVersion: [''],
       administratorId: ['', Validators.required],
-      active: [true, Validators.required]
+      active: [true, Validators.required],
     });
   }
-  get firstName(){
+  get firstName() {
     return this.newUserForm.get('firstName');
   }
-  get lastName(){
+  get lastName() {
     return this.newUserForm.get('lastName');
   }
-  get username(){
+  get username() {
     return this.newUserForm.get('username');
   }
-  get displayName(){
+  get displayName() {
     return this.newUserForm.get('displayName');
   }
-  get email(){
+  get email() {
     return this.newUserForm.get('email');
   }
-  get managerId(){
+  get managerId() {
     return this.newUserForm.get('managerId');
   }
-  get type(){
+  get type() {
     return this.newUserForm.get('type');
   }
-  get userId(){
+  get userId() {
     return this.newUserForm.get('userId');
   }
-  get password(){
+  get password() {
     return this.newUserForm.get('password');
   }
-  get softwareVersion(){
+  get softwareVersion() {
     return this.newUserForm.get('softwareVersion');
   }
-  get aministratorId(){
+  get aministratorId() {
     return this.newUserForm.get('administratorId');
   }
-  get active(){
+  get active() {
     return this.newUserForm.get('active');
   }
 
@@ -82,18 +107,15 @@ export class NewUserFormComponent {
     });
   }
 
-  createUser(){
-    this.httpService.createUser(this.newUserForm.value).subscribe(
-      {
-        next: resp => {
-          console.log(resp.body)
-        },
-        error: err => {
-          console.log(err)
-          console.log(this.newUserForm.value)
-        }
-      }
-    );
+  createUser() {
+    this.httpService.createUser(this.newUserForm.value).subscribe({
+      next: (resp) => {
+        console.log(resp.body);
+      },
+      error: (err) => {
+        console.log(err);
+        console.log(this.newUserForm.value);
+      },
+    });
   }
-
 }
