50f24e8b59fe7397ecdf2c4567d57db0128bc419
DustinYansberg <DustinYansberg@gmail.com>
2024-08-08 20:15:23 -0700

    we did it

--- angular/personnel-management-2-fe/src/app/services/http.service.ts
+++ angular/personnel-management-2-fe/src/app/services/http.service.ts
@@ -2,68 +2,94 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
+import { Account } from '../models/account';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  url: String ='http://localhost:8081/';
+  url: String = 'http://localhost:8081/';
 
   //get all users
-  getAllUsers(): Observable<HttpResponse<any>>{
-    return this.http.get(this.url+'identity', {observe: 'response'});
+  getAllUsers(): Observable<HttpResponse<any>> {
+    return this.http.get(this.url + 'identity', { observe: 'response' });
   }
 
   //get user by id
-  getUserById(id: string): Observable<HttpResponse<any>>{
-    return this.http.get(this.url+'identity/'+id, {observe: 'response'});
+  getUserById(id: string): Observable<HttpResponse<any>> {
+    return this.http.get(this.url + 'identity/' + id, { observe: 'response' });
   }
 
   //get account
-  getAccountById(id: string): Observable<HttpResponse<any>>{
-      return this.http.get(this.url +'account/'+id, {observe: 'response'})
+  getAccountById(id: string): Observable<HttpResponse<any>> {
+    return this.http.get(this.url + 'account/' + id, { observe: 'response' });
   }
   //create user
-  createUser(user: User){
-    console.log("create user request", user);
-    return this.http.post(this.url+'identity', {
-      "userName": user.username,
-      "password": user.password,
-      "firstName": user.firstName,
-      "lastName": user.lastName,
-      "email": user.email,
-      "administratorId": "0a03000490e613798190e663d56f00ea",
-      "displayName": user.displayName,
-      "active": user.active,
-      "userType": "employee",
-      "department": user.department
-  }, {observe: 'response'});
+  createUser(user: User) {
+    return this.http.post(
+      this.url + 'identity',
+      {
+        userName: user.username,
+        password: user.password,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        administratorId: '0a03000490e613798190e663d56f00ea',
+        displayName: user.displayName,
+        active: user.active,
+        userType: 'employee',
+        department: user.department,
+      },
+      { observe: 'response' }
+    );
   }
 
+  //create account
+  createAccount(account: Account) {
+    console.log(account);
+    return this.http.post(this.url + 'account', account, {
+      observe: 'response',
+    });
+  }
   //Edit user
-  updateUser(user: User){
-    console.log("update user request", user, this.url+'identity/'+ user.userId)
-    return this.http.put(this.url+'identity/'+ user.userId, {
-      "userName": user.username,
-      "password": user.password,
-      "firstName": user.firstName,
-      "lastName": user.lastName,
-      "email": user.email,
-      "administratorId": "0a03000490e613798190e663d56f00ea",
-      "displayName": user.displayName,
-      "active": user.active,
-      "userType": 'employee',
-      "department": user.department,
-      "managerId": user.managerId,
-  }, {observe: 'response'});
+  updateUser(user: User) {
+    console.log(
+      'update user request',
+      user,
+      this.url + 'identity/' + user.userId
+    );
+    return this.http.put(
+      this.url + 'identity/' + user.userId,
+      {
+        userName: user.username,
+        password: user.password,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        administratorId: '0a03000490e613798190e663d56f00ea',
+        displayName: user.displayName,
+        active: user.active,
+        userType: 'employee',
+        department: user.department,
+        managerId: user.managerId,
+      },
+      { observe: 'response' }
+    );
   }
 
   //delete user by ID
-  deleteUser(id: string): Observable<HttpResponse<any>>{
-    console.log("delete user " + id)
-    return this.http.delete(this.url +'identity/' + id, {observe: 'response'});
+  deleteUser(id: string): Observable<HttpResponse<any>> {
+    console.log('delete user ' + id);
+    return this.http.delete(this.url + 'identity/' + id, {
+      observe: 'response',
+    });
+  }
+
+  deleteAccount(accountId: string) {
+    return this.http.delete(this.url + 'account/' + accountId, {
+      observe: 'response',
+    });
   }
 }
--- angular/personnel-management-2-fe/src/app/user-page/user-page.component.ts
+++ angular/personnel-management-2-fe/src/app/user-page/user-page.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { HttpService } from '../services/http.service';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../models/user';
 import { CardModule } from 'primeng/card';
@@ -9,51 +16,206 @@ import { Account } from '../models/account';
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [CardModule, ButtonModule],
+  imports: [FormsModule, ReactiveFormsModule, CardModule, ButtonModule],
   templateUrl: './user-page.component.html',
-  styleUrl: './user-page.component.css'
+  styleUrl: './user-page.component.css',
 })
 export class UserPageComponent {
-
-  constructor(private httpService:HttpService, private route: ActivatedRoute){
-    this.getUserById()
+  newAccountForm: FormGroup;
+  constructor(
+    private httpService: HttpService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {
+    this.getUserById();
+    this.newAccountForm = this.formBuilder.group({
+      accountAppId: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(4)]),
+      ],
+      accountUserId: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(4)]),
+      ],
+      nativeIdentity: [''],
+      displayName: ['', Validators.required],
+      instanceId: [''],
+      password: [''],
+      currentPassword: ['', Validators.required],
+      active: [true],
+      locked: [false],
+      manuallyCorrelated: [false],
+      hasEntitlements: [true],
+      accountAppName: ['', Validators.required],
+      salesforceUsername: ['', Validators.required],
+      salesforceFirstName: ['', Validators.required],
+      salesforceLastName: ['', Validators.required],
+      salesforceCommunityNickname: ['', Validators.required],
+      salesforceAlias: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(8),
+        ]),
+      ],
+      salesforceEmail: ['', Validators.required],
+    });
+  }
+  isAccountBlockOpen: boolean = false;
+  account: Account = new Account(
+    '0a03000490e613798190f06722171485',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    true,
+    false,
+    false,
+    false,
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    ''
+  );
+  user: User = new User(
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    true,
+    ''
+  );
+  userAccounts: Account[] = [];
+  getUserById() {
+    this.httpService
+      .getUserById(this.route.snapshot.params['id'])
+      .subscribe((resp) => {
+        let item = resp.body;
+        const dynamicKey =
+          'urn:ietf:params:scim:schemas:sailpoint:1.0:User' as keyof typeof item;
+        const adminValue = item[dynamicKey];
+        const dynamicKey2 =
+          'urn:ietf:params:scim:schemas:extension:enterprise:2.0:User' as keyof typeof item;
+        const managerValue = item[dynamicKey2];
+        console.log(item);
+        this.user = new User(
+          item.id,
+          item.userName,
+          'password',
+          item.name.givenName,
+          item.name.familyName,
+          item.displayName,
+          item.emails[0].value,
+          managerValue.manager.displayId,
+          managerValue.manager.value,
+          item.meta.resourceType,
+          item.meta.version,
+          adminValue.administrator.displayName,
+          adminValue.administrator.value,
+          true,
+          'department'
+        );
+        const dynamicKey3 =
+          'urn:ietf:params:scim:schemas:sailpoint:1.0:User' as keyof typeof item;
+        const accountValue = item[dynamicKey3];
+        this.getUserAccountIds(accountValue.accounts);
+      });
+  }
+  ngOnInit() {
+    this.newAccountForm.valueChanges.subscribe((formValues) => {
+      this.account = { ...this.account, ...formValues };
+    });
+    this.newAccountForm.patchValue(this.account);
   }
 
-  user: User = new User('', '', '', '', '', '', '', '', '', '', '','','',true,'');
-  userAccounts: Account[]=[];
-  getUserById() {
-    this.httpService.getUserById(this.route.snapshot.params['id']).subscribe(resp => {
-      let item =resp.body;
-      const dynamicKey = "urn:ietf:params:scim:schemas:sailpoint:1.0:User" as keyof typeof item;
-      const adminValue = item[dynamicKey];
-      const dynamicKey2 = "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User" as keyof typeof item;
-      const managerValue = item[dynamicKey2];
-      console.log(item)
-      this.user =new User(item.id, item.userName, 'password', item.name.givenName, item.name.familyName, item.displayName, item.emails[0].value, managerValue.manager.displayId, managerValue.manager.value, item.meta.resourceType, item.meta.version, adminValue.administrator.displayName, adminValue.administrator.value, true, 'department');
-      const dynamicKey3 = "urn:ietf:params:scim:schemas:sailpoint:1.0:User" as keyof typeof item;
-      const accountValue = item[dynamicKey3];
-      this.getUserAccountIds(accountValue.accounts);
+  openAccountBlock() {
+    console.log('account block opened');
+    this.account.accountUserId = this.user.userId;
+    this.account.password = 'password';
+    this.account.currentPassword = 'password';
+    this.account.accountAppName = 'Salesforce';
+    this.newAccountForm.patchValue(this.account);
+    this.isAccountBlockOpen = !this.isAccountBlockOpen;
+    // open a new block with a form for creating a new account with this user
+  }
+  createAccount() {
+    this.httpService.createAccount(this.newAccountForm.value).subscribe({});
+  }
+
+  deleteAccount(accountId: string) {
+    this.httpService.deleteAccount(accountId).subscribe({
+      next: (resp) => {
+        console.log(resp);
+      },
+      error: (err) => {
+        console.log(err);
+      },
     });
   }
 
-  getUserAccountIds(accounts:any){
-      for (let account of accounts){
-        console.log("account "+account.value)
-        this.httpService.getAccountById(account.value).subscribe(
-          {
-            next: resp => {
-              let item = resp.body;
-              console.log(item)
-              const dynamicKey5 = "urn:ietf:params:scim:schemas:sailpoint:1.0:Application:Schema:Salesforce:account" as keyof typeof item;
-      const accountNestedInfo = item[dynamicKey5];
-      console.log(accountNestedInfo)
-          this.userAccounts.push(new Account(item.application.value, item.identity.value, item.nativeIdentity, item.identity.displayName, item.id, 'password','password', item.active, item.locked, item.manuallyCorrelated, item.hasEntitlements, item.application.displayName, accountNestedInfo.Username, accountNestedInfo.FirstName, accountNestedInfo.LastName, accountNestedInfo.CommunityNickname, accountNestedInfo.Alias, accountNestedInfo.Email));
-            },
-            error: err => {
-              console.log(err);
-            }
-          });
-      }
-      console.log(this.userAccounts);
+  getUserAccountIds(accounts: any) {
+    for (let account of accounts) {
+      console.log('account ' + account.value);
+      this.httpService.getAccountById(account.value).subscribe({
+        next: (resp) => {
+          let item = resp.body;
+          console.log(item);
+          let accountNestedInfo;
+          if (item.application.displayName == 'Azure Entra ID') {
+            const dynamicKey6 =
+              'urn:ietf:params:scim:schemas:sailpoint:1.0:Application:Schema:Azure Entra ID:account' as keyof typeof item;
+            accountNestedInfo = item[dynamicKey6];
+          }
+          if (item.application.displayName == 'Salesforce') {
+            const dynamicKey5 =
+              'urn:ietf:params:scim:schemas:sailpoint:1.0:Application:Schema:Salesforce:account' as keyof typeof item;
+
+            accountNestedInfo = item[dynamicKey5];
+          }
+          console.log(accountNestedInfo);
+          this.userAccounts.push(
+            new Account(
+              item.application.value,
+              item.identity.value,
+              item.nativeIdentity,
+              item.identity.displayName,
+              item.id,
+              'password',
+              'password',
+              item.active,
+              item.locked,
+              item.manuallyCorrelated,
+              item.hasEntitlements,
+              item.application.displayName,
+              accountNestedInfo.Username,
+              accountNestedInfo.FirstName,
+              accountNestedInfo.LastName,
+              accountNestedInfo.CommunityNickname,
+              accountNestedInfo.Alias,
+              accountNestedInfo.Email
+            )
+          );
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+    console.log(this.userAccounts);
   }
 }
