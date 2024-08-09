9cc918704c259e3db144ceaa573c5aef85ea9bcf
DustinYansberg <DustinYansberg@gmail.com>
2024-08-09 05:23:11 -0700

    fixing sort functionality. It was broken because of the identities without a first and last name. adding optional denotion to the firstName and lastName properties in the sort methods to fix.

--- angular/personnel-management-2-fe/src/app/users/users.component.ts
+++ angular/personnel-management-2-fe/src/app/users/users.component.ts
@@ -7,90 +7,108 @@ import { FormsModule } from '@angular/forms';
 import { FilterService, PrimeIcons } from 'primeng/api';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-users',
   standalone: true,
   imports: [UserCardComponent, Button, FormsModule],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
 export class UsersComponent {
-users: User[] = [];
-searchStr: string = '';
+  users: User[] = [];
+  searchStr: string = '';
 
-  constructor(private httpService: HttpService, private filterService: FilterService, private router: Router){
+  constructor(
+    private httpService: HttpService,
+    private filterService: FilterService,
+    private router: Router
+  ) {
     this.getAllUsers();
   }
-  getAllUsers(){
+  getAllUsers() {
     //this.users = this.httpService.getAllUsers();
-    this.httpService.getAllUsers().subscribe(resp=>{
+    this.httpService.getAllUsers().subscribe((resp) => {
       this.users = [];
-      for (let item of resp.body['Resources']){
-        if(item.displayName){
-          const dynamicKey = "urn:ietf:params:scim:schemas:sailpoint:1.0:User" as keyof typeof item;
+      for (let item of resp.body['Resources']) {
+        if (item.displayName) {
+          const dynamicKey =
+            'urn:ietf:params:scim:schemas:sailpoint:1.0:User' as keyof typeof item;
           const adminValue = item[dynamicKey];
-          const dynamicKey2 = "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User" as keyof typeof item;
+          const dynamicKey2 =
+            'urn:ietf:params:scim:schemas:extension:enterprise:2.0:User' as keyof typeof item;
           const managerValue = item[dynamicKey2];
-        this.users.push(
-          new User(item.id, item.userName, 'password', item.name.givenName, item.name.familyName, item.displayName, item.emails[0].value, managerValue.manager.displayId, managerValue.manager.value, item.userType, item.meta.version, adminValue.administrator.displayName, adminValue.administrator.value, true, 'department')
-        );
-     }
+          this.users.push(
+            new User(
+              item.id,
+              item.userName,
+              'password',
+              item.name.givenName,
+              item.name.familyName,
+              item.displayName,
+              item.emails[0].value,
+              managerValue.manager.displayId,
+              managerValue.manager.value,
+              item.userType,
+              item.meta.version,
+              adminValue.administrator.displayName,
+              adminValue.administrator.value,
+              true,
+              'department'
+            )
+          );
+        }
       }
-  })
+    });
   }
 
-  deleteUser(userId: string){
-    this.httpService.deleteUser(userId)
-    .subscribe(response =>{
+  deleteUser(userId: string) {
+    this.httpService.deleteUser(userId).subscribe((response) => {
       this.getAllUsers();
     });
-    
   }
 
-  processDeleteEvent(userId: string){
+  processDeleteEvent(userId: string) {
     this.deleteUser(userId);
   }
 
   //Sorting
-  sortLastAZ(){
-    this.users = this.users.sort((a,b) =>
-      a.lastName.localeCompare(b.lastName)
+  sortLastAZ() {
+    this.users = this.users.sort((a, b) =>
+      a.lastName?.localeCompare(b.lastName)
     );
   }
 
-  sortLastZA(){
-    this.users = this.users.sort((a,b) =>
-      b.lastName.localeCompare(a.lastName)
+  sortLastZA() {
+    this.users = this.users.sort((a, b) =>
+      b.lastName?.localeCompare(a.lastName)
     );
   }
 
-  sortFirstAZ(){
-    this.users = this.users.sort((a,b) =>
-      a.firstName.localeCompare(b.firstName)
+  sortFirstAZ() {
+    this.users = this.users.sort((a, b) =>
+      a.firstName?.localeCompare(b.firstName)
     );
   }
 
-  sortFirstZA(){
-    this.users = this.users.sort((a,b) =>
-      b.firstName.localeCompare(a.firstName)
+  sortFirstZA() {
+    this.users = this.users.sort((a, b) =>
+      b.firstName?.localeCompare(a.firstName)
     );
   }
 
-
-  search(){
+  search() {
     // let str = this.searchStr
     // this.users =this.users.filter(user => {
-    //   return user.firstName.toLocaleLowerCase().startsWith(str.toLocaleLowerCase()) 
-    //   || user.lastName.toLocaleLowerCase().startsWith(str.toLocaleLowerCase()) 
+    //   return user.firstName.toLocaleLowerCase().startsWith(str.toLocaleLowerCase())
+    //   || user.lastName.toLocaleLowerCase().startsWith(str.toLocaleLowerCase())
     // });
   }
-  reset(){
+  reset() {
     this.getAllUsers();
-    this.searchStr ='';
+    this.searchStr = '';
   }
 
-  clicked(id: string){
-    this.router.navigate(['user/'+id]);
+  clicked(id: string) {
+    this.router.navigate(['user/' + id]);
   }
 }
