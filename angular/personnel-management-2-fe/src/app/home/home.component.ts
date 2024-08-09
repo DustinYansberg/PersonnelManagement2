88634245f318df4b49789e28cd3745ddb31c83d6
DustinYansberg <DustinYansberg@gmail.com>
2024-08-08 17:10:58 -0700

    home page fleshed out a bit

--- angular/personnel-management-2-fe/src/app/home/home.component.ts
+++ angular/personnel-management-2-fe/src/app/home/home.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { RouterLink } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+import { BadgeModule } from 'primeng/badge';
+import { MenubarModule } from 'primeng/menubar';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TagModule, RouterLink, MenubarModule, BadgeModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+items: MenuItem[] = [];
+  numOfUsers:number = 0;
+  constructor(private service:HttpService) { 
+  }
+  ngOnInit(){
+    this.getAccounts();
+    this.items = [{
+      label: 'Home',
+      // icon:'pi pi-fw pi-home',
+      route: ['/']
+    }, {
+      label: 'Users',
+      // icon:'pi pi-fw pi-home',
+      route: ['/users']
+    }, 
+    {
+      label: 'Create',
+      // icon:'pi pi-fw pi-home',
+      route: ['/create']
+    }]
+      }
+
+  getAccounts() {
+    this.service.getAllUsers().subscribe(resp => {
+      this.numOfUsers = resp.body.totalResults;
+      
+    });
+  }
+
 
 }
